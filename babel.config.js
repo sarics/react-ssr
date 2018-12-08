68a0f989ef760162ff0083c960885d80b4d48fdd
sarics <sari.csaba@outlook.hu>
2018-12-08 08:25:51 +0100

    use babel.config.js instead of .babelrc

---
+++ babel.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        modules: false,
+      },
+    ],
+    '@babel/preset-react',
+  ],
+  plugins: [
+    '@babel/plugin-syntax-dynamic-import',
+    '@babel/plugin-proposal-object-rest-spread',
+  ],
+};
